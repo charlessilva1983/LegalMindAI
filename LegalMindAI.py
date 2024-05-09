7ff4ee5781794e20d33036211829e567139009d8
RPConsultoria <107998578+RPConsultoria@users.noreply.github.com>
2024-05-09 20:07:23 -0300

    Update .env file

--- LegalMindAI.py
+++ LegalMindAI.py
@@ -2,8 +2,13 @@ import requests
 from pathlib import Path
 import hashlib
 import google.generativeai as genai
+import os
+from dotenv import load_dotenv
 
-API_KEY = ''
+load_dotenv()
+
+API_KEY = os.getenv('API_KEY')
+
 genai.configure(api_key=API_KEY)
 
 generation_config = {
